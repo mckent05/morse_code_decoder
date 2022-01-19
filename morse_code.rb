5be4ae716d19451557ce96d037a386a8a0b34483
MCKENT05 <atemitope95@gmail.com>
2022-01-19 17:01:58 +0100

    fixed linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -1,39 +1,39 @@
 def decode_letter(letter)
-    morse_decode = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-                      '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-                      '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-                      '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-                      '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-                      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-                      '-.--' => 'Y', '--..' => 'Z' }
-    return morse_decode[letter]
+  morse_decode = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+                   '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+                   '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+                   '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+                   '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+                   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+                   '-.--' => 'Y', '--..' => 'Z' }
+  morse_decode[letter]
 end
 
 def decode_word(words)
-    get_letter = ''
-    form_word = ''
-    word = words.split
-    word.each do |letter|
-       if letter ==='/'
-           get_letter = ' '
-       else
-           get_letter = decode_letter(letter)
-       end
-       form_word +=get_letter
-    end
-    return form_word
+  get_letter = ''
+  form_word = ''
+  word = words.split
+  word.each do |letter|
+    get_letter = if letter == '/'
+                   ' '
+                 else
+                   decode_letter(letter)
+                 end
+    form_word += get_letter
+  end
+  form_word
 end
 
 def decode_message(message)
-    get_word =''
-    form_message = ''
-    message=message.split('   ')
-    message.each do |word| 
-       get_word = decode_word(word)
-       form_message +="#{get_word} "
-    end
-    return form_message
+  get_word = ''
+  form_message = ''
+  message = message.split('   ')
+  message.each do |word|
+    get_word = decode_word(word)
+    form_message += "#{get_word} "
+  end
+  form_message
 end
 
-puts decode_message("-- -.--   -. .- -- .")
+puts decode_message('-- -.--   -. .- -- .')
 puts decode_message('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
